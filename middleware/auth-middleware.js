df144e69a29202771c07308cc0f83fee55772417
Muhammad Taimoor Sultani <team.dev.man@gmail.com>
2020-10-27 21:11:32 +0500

    forget password and correction in middleware

--- middleware/auth-middleware.js
+++ middleware/auth-middleware.js
@@ -2,10 +2,24 @@ var firebase = require("firebase");
 
 module.exports = () => {
   return (req, res, next) => {
-    console.log("autherization middleware");
     if (firebase.auth().currentUser !== null) {
-      next();
+      if (
+        req.originalUrl === "/auth/login" ||
+        req.originalUrl === "/auth/signup" ||
+        req.originalUrl === "/auth/forgotPassword"
+      ) {
+        return res.redirect("/");
+      } else {
+        next();
+      }
     } else {
+      if (
+        req.originalUrl === "/auth/login" ||
+        req.originalUrl === "/auth/signup" ||
+        req.originalUrl === "/auth/forgotPassword"
+      ) {
+        next();
+      }
       return res.redirect("/auth/login");
     }
   };
--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -3,61 +3,91 @@ var firebase = require("firebase");
 var router = express.Router();
 const authorize = require("../../middleware/auth-middleware");
 
-router.get("/login", function(req, res, next) {
-    res.render("pages/auth/login", {
-        title: "Express",
+router.get("/login", authorize(), function (req, res, next) {
+  res.render("pages/auth/login", {
+    isLogedIn: req.session.isLogedIn,
+    error: "",
+  });
+});
+
+router.post("/login", function (req, res, next) {
+  const userEmail = req.body.userEmail;
+  const userPassword = req.body.userPassword;
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(userEmail, userPassword)
+    .then((result) => {
+      req.session.isLogedIn = true;
+      res.redirect("/");
+    })
+    .catch((err) => {
+      res.render("pages/auth/login", {
         isLogedIn: req.session.isLogedIn,
+        error: err.message,
+      });
     });
 });
 
-router.post("/login", function(req, res, next) {
-    const userEmail = req.body.userEmail;
-    const userPassword = req.body.userPassword;
-    firebase
-        .auth()
-        .signInWithEmailAndPassword(userEmail, userPassword)
-        .then((result) => {
-            req.session.isLogedIn = true;
-            res.redirect("/");
-        })
-        .catch((err) => {
-            res.send(err);
-        });
+router.get("/signup", authorize(), function (req, res, next) {
+  res.render("pages/auth/signUp", {
+    isLogedIn: req.session.isLogedIn,
+    error: "",
+  });
 });
 
-router.get("/signup", function(req, res, next) {
-    res.render("pages/auth/signUp", {
-        title: "Express",
+router.post("/signup", function (req, res, next) {
+  // firebase.auth().sendPasswordResetEmail();
+  const userEmail = req.body.userEmail;
+  const userPassword = req.body.userPassword;
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(userEmail, userPassword)
+    .then((result) => {
+      req.session.isLogedIn = true;
+      res.redirect("/");
+    })
+    .catch((err) => {
+      res.render("pages/auth/signUp", {
         isLogedIn: req.session.isLogedIn,
+        error: err.message,
+      });
     });
 });
 
-router.post("/signup", function(req, res, next) {
-    const userEmail = req.body.userEmail;
-    const userPassword = req.body.userPassword;
-    firebase
-        .auth()
-        .createUserWithEmailAndPassword(userEmail, userPassword)
-        .then((result) => {
-            req.session.isLogedIn = true;
-            res.redirect("/");
-        })
-        .catch((err) => {
-            res.send(err);
-        });
+router.get("/signout", authorize(), function (req, res, next) {
+  firebase
+    .auth()
+    .signOut()
+    .then(function () {
+      req.session.isLogedIn = false;
+      res.redirect("/");
+    })
+    .catch(function (error) {
+      // An error happened.
+    });
 });
 
-router.get("/signout", authorize(), function(req, res, next) {
-    firebase
-        .auth()
-        .signOut()
-        .then(function() {
-            req.session.isLogedIn = false;
-            res.redirect("/");
-        })
-        .catch(function(error) {
-            // An error happened.
-        });
+router.get("/forgotPassword", authorize(), function (req, res, next) {
+  res.render("pages/auth/forgotPassword", {
+    isLogedIn: req.session.isLogedIn,
+    error: "",
+  });
+});
+
+router.post("/forgotPassword", function (req, res, next) {
+  const userEmail = req.body.userEmail;
+  firebase
+    .auth()
+    .sendPasswordResetEmail(userEmail)
+    .then((result) => {
+      res.redirect("/");
+    })
+    .catch((err) => {
+      res.render("pages/auth/forgotPassword", {
+        isLogedIn: req.session.isLogedIn,
+        error: err.message,
+      });
+    });
 });
 
 module.exports = router;
